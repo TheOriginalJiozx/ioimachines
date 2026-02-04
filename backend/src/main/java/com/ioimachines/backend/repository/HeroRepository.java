8908558b6af7c468c3a130e9ed83804b2a8ee0e9
TheOriginalJiozx <omal0001@stud.ek.dk>
2026-02-04 22:26:35 +0100

    fjernet meget... db content var ikke en god nok idé

--- backend/src/main/java/com/ioimachines/backend/repository/HeroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ioimachines.backend.repository;
-
-import com.ioimachines.backend.model.Hero;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HeroRepository extends JpaRepository<Hero, Long> {
-}
