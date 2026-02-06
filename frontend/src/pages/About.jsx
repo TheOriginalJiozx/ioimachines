261b542b6c4f77a82d7aa787629d6c4497cad5eb
TheOriginalJiozx <omal0001@stud.ek.dk>
2026-02-06 17:04:07 +0100

    unused import removed

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import ContactCase from "../components/ContactCase";
 import Features from "../components/Features";
 import GetAdvice from "../components/GetAdvice";
